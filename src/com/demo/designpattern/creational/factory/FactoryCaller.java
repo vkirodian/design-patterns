5271eee685726af10a683ca2dca0fe33e83f088a
VK <vijesh_kirodian@hotmail.com>
2024-07-12 16:50:18 +0530

    Added javadocs

--- src/com/demo/designpattern/creational/factory/FactoryCaller.java
+++ src/com/demo/designpattern/creational/factory/FactoryCaller.java
@@ -1,9 +1,14 @@
 package com.demo.designpattern.creational.factory;
 
-//Caller class
+/**
+ * Demonstrates the use of the Factory Design Pattern in creating Car objects.
+ * This class serves as a client that calls the CarFactory to create instances of Car
+ * based on predefined types (Hatchback, Sedan, SUV). It showcases how the factory pattern
+ * abstracts the instantiation logic, allowing for easy creation of objects without
+ * needing to know the specific classes required for each car type.
+ */
 public class FactoryCaller {
 
-	// See how we just provide the car type to the factory
 	public static void call() {
 		System.out.println("***Factory***");
 		System.out.println(CarFactory.constructCar(CarType.Hatchback));
