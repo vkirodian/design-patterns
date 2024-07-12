b4b883b34a02ae5d7a1f01b18aee172f216fd560
VK <vijesh_kirodian@hotmail.com>
2024-07-12 16:01:33 +0530

    Fixed builder pattern code

--- src/com/demo/designpattern/creational/builder/BuilderCaller.java
+++ src/com/demo/designpattern/creational/builder/BuilderCaller.java
@@ -1,19 +1,24 @@
 package com.demo.designpattern.creational.builder;
 
-//Caller class.
+/**
+ * The {@code BuilderCaller} class demonstrates the use of the Builder design pattern.
+ * It showcases how to construct complex objects step by step using different builder implementations.
+ * The class contains a static method {@code call()} that creates complex objects by directing
+ * the construction process through the {@code HouseBuilder} class, using different builders
+ * for constructing various types of houses (e.g., apartments, cellars).
+ */
 public class BuilderCaller {
 
 	public static void call() {
-		// See how we pass the builder implementation to directory class, this would
-		// give us an fully initialized complex object for that particular type.
 		System.out.println("***Builder***");
-		HouseBuildingDirector hbda = new HouseBuildingDirector(new ApartmentBuilder());
-		HouseBuildingDirector hbdc = new HouseBuildingDirector(new CellerBuilder());
+		HouseBuilder apartmentBuilder = new HouseBuilder();
+		apartmentBuilder.setFloor("Concrete").setWalls("Wood").setRoof("Shingles")
+				.setDoors("Wood").setWindows("Glass");
+		System.out.println(apartmentBuilder.buildHouse());
 
-		hbda.construct();
-		hbdc.construct();
-
-		System.out.println(hbda.getHouse());
-		System.out.println(hbdc.getHouse());
+		HouseBuilder cellarBuilder = new HouseBuilder();
+		cellarBuilder.setFloor("Dirt").setWalls("Stone").setRoof("Stone")
+				.setDoors("Metal");
+		System.out.println(cellarBuilder.buildHouse());
 	}
 }
