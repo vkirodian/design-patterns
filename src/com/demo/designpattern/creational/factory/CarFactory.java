23dbfb35d11ea1ee95cbf9dc4d424362443930b4
VK <vijesh_kirodian@hotmail.com>
2024-07-12 16:48:25 +0530

    Added javadocs

--- src/com/demo/designpattern/creational/factory/CarFactory.java
+++ src/com/demo/designpattern/creational/factory/CarFactory.java
@@ -1,9 +1,19 @@
 package com.demo.designpattern.creational.factory;
 
-//Factory for cars
+/**
+ * Factory class for creating instances of Car.
+ * This class follows the Factory Design Pattern to abstract the instantiation logic of Car objects.
+ * It provides a static method to create a Car instance based on the CarType.
+ */
 public class CarFactory {
 
-	//Depending on the type of car passed, we can create corresponding car class.
+	/**
+     	* Creates and returns an instance of a Car based on the specified CarType.
+     	* 
+     	* @param type The type of car to create (Hatchback, Sedan, SUV).
+     	* @return Car An instance of a Car subclass corresponding to the specified type.
+     	* @throws RuntimeException if an invalid CarType is passed.
+     	*/
 	public static Car constructCar(CarType type) {
 		Car car;
 		switch (type) {
