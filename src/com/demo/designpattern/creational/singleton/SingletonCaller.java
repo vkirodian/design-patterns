32d11a14e0dfe315dd098f2309230e800a4be533
VK <vijesh_kirodian@hotmail.com>
2024-07-12 15:58:48 +0530

    Added javadocs

--- src/com/demo/designpattern/creational/singleton/SingletonCaller.java
+++ src/com/demo/designpattern/creational/singleton/SingletonCaller.java
@@ -1,6 +1,10 @@
 package com.demo.designpattern.creational.singleton;
 
-//Caller class.
+/**
+ * The {@code SingletonCaller} class is responsible for demonstrating the Singleton design pattern.
+ * It contains a static method {@code call()} that prints the hash codes of the Singleton instance
+ * to illustrate that each call returns the same object instance, showcasing the Singleton behavior.
+ */
 public class SingletonCaller {
 
 	public static void call() {
