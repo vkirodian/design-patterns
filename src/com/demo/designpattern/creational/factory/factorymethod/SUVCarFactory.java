73e40aaf0895fa400ccfa33a6bfced83b646436f
VK <vijesh_kirodian@hotmail.com>
2024-07-14 12:33:50 +0530

    Update SUVCarFactory.java

--- src/com/demo/designpattern/creational/factory/factorymethod/SUVCarFactory.java
+++ src/com/demo/designpattern/creational/factory/factorymethod/SUVCarFactory.java
@@ -3,6 +3,11 @@ package com.demo.designpattern.creational.factory.factorymethod;
 import com.demo.designpattern.creational.factory.Car;
 import com.demo.designpattern.creational.factory.SUVCar;
 
+/**
+ * Concrete factory for creating SUVCar objects.
+ * Extends the AbstractCarFactory to implement the factory method pattern,
+ * specifically for creating instances of SUVCar.
+ */
 public class SUVCarFactory extends AbstractCarFactory{
     @Override
     public Car constructCar() {
