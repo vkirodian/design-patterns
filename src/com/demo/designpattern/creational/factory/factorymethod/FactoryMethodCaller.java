63f0450ec78b63a835f85a60e48abf3fa4d9614b
VK <vijesh_kirodian@hotmail.com>
2024-07-14 12:31:39 +0530

    Added javadocs

--- src/com/demo/designpattern/creational/factory/factorymethod/FactoryMethodCaller.java
+++ src/com/demo/designpattern/creational/factory/factorymethod/FactoryMethodCaller.java
@@ -1,5 +1,12 @@
 package com.demo.designpattern.creational.factory.factorymethod;
 
+/**
+ * Demonstrates the use of the Factory Method Design Pattern in creating Car objects.
+ * This class serves as a client that calls specific CarFactory implementations to create instances of Car
+ * (SedanCar, SUVCar, HatchbackCar) and then drives them. It showcases how the factory method pattern
+ * abstracts the instantiation logic into separate factories, allowing for easy creation and manipulation
+ * of objects without needing to know the specific classes required for each car type.
+ */
 public class FactoryMethodCaller {
 
     public static void call() {
