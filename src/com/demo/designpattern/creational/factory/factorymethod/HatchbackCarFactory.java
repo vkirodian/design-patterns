5d74760968edcfc85f382d95cc1657e774d84a31
VK <vijesh_kirodian@hotmail.com>
2024-07-14 12:33:25 +0530

    Update HatchbackCarFactory.java

--- src/com/demo/designpattern/creational/factory/factorymethod/HatchbackCarFactory.java
+++ src/com/demo/designpattern/creational/factory/factorymethod/HatchbackCarFactory.java
@@ -3,6 +3,11 @@ package com.demo.designpattern.creational.factory.factorymethod;
 import com.demo.designpattern.creational.factory.Car;
 import com.demo.designpattern.creational.factory.HatchbackCar;
 
+/**
+ * Concrete factory for creating HatchbackCar objects.
+ * Extends the AbstractCarFactory to implement the factory method pattern,
+ * specifically for creating instances of HatchbackCar.
+ */
 public class HatchbackCarFactory extends AbstractCarFactory{
     @Override
     public Car constructCar() {
