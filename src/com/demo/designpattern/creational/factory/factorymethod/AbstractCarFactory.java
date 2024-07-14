b6736fda661e6d118d7662a56527db28bc69f96a
VK <vijesh_kirodian@hotmail.com>
2024-07-14 12:32:45 +0530

    Added javadocs

--- src/com/demo/designpattern/creational/factory/factorymethod/AbstractCarFactory.java
+++ src/com/demo/designpattern/creational/factory/factorymethod/AbstractCarFactory.java
@@ -2,6 +2,12 @@ package com.demo.designpattern.creational.factory.factorymethod;
 
 import com.demo.designpattern.creational.factory.Car;
 
+/**
+ * Abstract factory class for creating Car objects.
+ * This class defines a common interface for creating various types of Car objects,
+ * allowing for the implementation of factory method pattern where each concrete factory
+ * class can decide how to instantiate Car objects.
+ */
 public abstract class AbstractCarFactory {
 
     public abstract Car constructCar();
