3f6d17e85a3737dad87748680f2b555757f68770
VK <vijesh_kirodian@hotmail.com>
2024-07-14 12:34:08 +0530

    Update SedanCarFactory.java

--- src/com/demo/designpattern/creational/factory/factorymethod/SedanCarFactory.java
+++ src/com/demo/designpattern/creational/factory/factorymethod/SedanCarFactory.java
@@ -3,6 +3,11 @@ package com.demo.designpattern.creational.factory.factorymethod;
 import com.demo.designpattern.creational.factory.Car;
 import com.demo.designpattern.creational.factory.SedanCar;
 
+/**
+ * Concrete factory for creating SedanCar objects.
+ * Extends the AbstractCarFactory to implement the factory method pattern,
+ * specifically for creating instances of SedanCar.
+ */
 public class SedanCarFactory extends AbstractCarFactory{
     @Override
     public Car constructCar() {
